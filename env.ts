a973cc8b3c33abc2cf0226675ce20b305212ab45
Leon <lelekram@gmail.com>
2024-05-20 18:27:00 +0200

    change topic env, remove unused env variable

--- env.ts
+++ env.ts
@@ -3,10 +3,8 @@ import 'dotenv/config.js'
 export const conf = {
     mqttHost: "192.168.2.180",
     mqttPort: "1883",
-    websitePort: 3000,
     mqttAlertTopicSubscribe : [
-        "Gebaeude1/Alarm/isActive",
-        "Test"
+        "Alarmanlage/Bewegungssensor/Haustür"
     ],
     mqttTopicAlertMessageOn: "true",
     mqttTopicAlertMessageOff: "false",
