6508b94dc1b663353e20a52fdce296eb0523040b
Leon <lelekram@gmail.com>
2024-05-20 18:05:01 +0200

    remove unused imports, added other env variable

--- env.ts
+++ env.ts
@@ -3,9 +3,12 @@ import 'dotenv/config.js'
 export const conf = {
     mqttHost: "192.168.2.180",
     mqttPort: "1883",
+    websitePort: 3000,
     mqttAlertTopicSubscribe : [
         "Gebaeude1/Alarm/isActive",
         "Test"
     ],
+    mqttTopicAlertMessageOn: "true",
+    mqttTopicAlertMessageOff: "false",
     alertMessagePhone: 'Warnung! Bewegung am Gebäude entdeckt!',
 }
--- index.ts
+++ index.ts
@@ -1,40 +1,34 @@
 import {conf} from "./env";
 import mqtt from "mqtt";
-
 const smsClient = require('twilio')(process.env.SMS_ACC_SID, process.env.SMS_AUTH_TOKEN);
 
-
+// configure host and connect to host
 const host = `mqtt://${conf.mqttHost}:${conf.mqttPort}`;
 const mqttClient = mqtt.connect(host);
 
+// logging for connecting to mqtt-broker
 mqttClient.on("connect", () => {
     console.log("Client connected");
 })
 
+// logging for errors with mqtt-client
 mqttClient.on("error", err => {
     console.log("Error: ", err);
 })
 
+// subscribe to every topic
 for (const topic of conf.mqttAlertTopicSubscribe) {
     mqttClient.subscribe(topic);
 }
 
-function configureReceiverSenders(envToSplit: string | undefined) {
-    if (envToSplit != null) {
-        return envToSplit.split(' ');
-    } else {
-        return ['']
-    }
-}
-
 // get and configure SMS receiver and sender
-const receiverPhones = configureReceiverSenders(process.env.SMS_PHONE_NUMBERS_RECEIVE)
+const receiverPhones = process.env.SMS_PHONE_NUMBERS_RECEIVE != null ? process.env.SMS_PHONE_NUMBERS_RECEIVE.split(' ') : [''];
 const senderPhone = process.env.SMS_NUMBER_SENDER;
 
-// send sms on incoming mqtt message alert
+// send sms on incoming mqtt message alert to every receiving phone in config
 mqttClient.on("message", (topic, message, _) => {
     console.log("Message: ", topic + " - " + message.toString());
-    if (message.toString() == "true") {
+    if (message.toString() == conf.mqttTopicAlertMessageOn) {
         console.log("alarm active", "sending messages");
         for (const tel of receiverPhones) {
             smsClient.messages.create({
@@ -42,10 +36,27 @@ mqttClient.on("message", (topic, message, _) => {
                 from: senderPhone,
                 to: tel
             }).then(() => {
-                console.log(`SMS send to ${tel}: ` + new Date().toLocaleString('de'))
+                console.log(`SMS send to ${tel}: ` + new Date().toLocaleString('de'));
             }).catch(() => {
-                console.log(`Error occurred sending to ${tel}: ` + new Date().toLocaleString('de'))
+                console.log(`Error occurred sending to ${tel}: ` + new Date().toLocaleString('de'));
             });
         }
     }
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
